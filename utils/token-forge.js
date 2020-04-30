1c6a664f51816139f2bf5067222abf6bf4877a24
Panday <benoit.fleuriaud@soyhuce.fr>
2020-04-30 11:51:48 +0200

    feat(token-forge): add a TokenForge class used to craft a token using 2 uuids

---
+++ utils/token-forge.js
@@ -0,0 +1,25 @@
+export default class TokenForge {
+  /**
+   * @param {string[]} uuids
+   */
+  constructor(uuids) {
+    if (uuids.length !== 2) {
+      throw new Error("The uuids param must have exactly 2 elements");
+    }
+
+    this._sortedUuids = [...uuids];
+    this._sortedUuids.sort();
+  }
+
+  /**
+   * Produces a SHA256 hash by concatenating the 2 uuids
+   */
+  async craft() {
+    const encoder = new TextEncoder();
+    const concatenatedUuidsData = encoder.encode(this._sortedUuids.join(""));
+    const hashBuffer = await crypto.subtle.digest("SHA-256", concatenatedUuidsData);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    return hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+  }
+}
