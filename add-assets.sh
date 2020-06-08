6539db05867e3baab2ebe4145c1e141bc9745fd6
Julien Roche <julien.roche@soyhuce.fr>
2020-06-08 17:11:45 +0200

    chore(*): add script to add assets

--- add-assets.sh
+++ add-assets.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-export NODE_ENV=production
-export BASE_URL=http://localhost:3000
-export API_HOST=https://api.fanny.soyhuce.fr
-export AUTHORIZATION_TOKEN=8im0qgacmw6w16uclemtk03oj477c2n2t8klpk64xfytgjev2d
-export PEER_SERVER_HOST=peer.fanny.soyhuce.fr
-export PEER_SERVER_PORT=443
-export PEER_SERVER_KEY=f1nda32t9oqr79pf5txl
-
 if [ -d "android" ]; then
   cp -R resources/android/assets/* android/app/src/main/res
   cp resources/android/AndroidManifest.xml android/app/src/main
@@ -19,5 +11,3 @@ if [ -d "ios" ]; then
   cp -R resources/ios/assets/* ios/App/App/Assets.xcassets
   cp resources/ios/Info.plist ios/App/App
 fi
-
-yarn build:native
